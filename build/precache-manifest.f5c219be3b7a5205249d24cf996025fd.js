88513043be17135a6b63d136a9ee37b9b9825c7f
Aisa <67589592+Aisa-Asghari@users.noreply.github.com>
2023-01-19 08:09:33 +0330

    Build for Netlify 👊🏻

---
+++ build/precache-manifest.f5c219be3b7a5205249d24cf996025fd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe20e8cd01f7921d5910",
+    "url": "/static/css/main.d520e4dd.chunk.css"
+  },
+  {
+    "revision": "fe20e8cd01f7921d5910",
+    "url": "/static/js/main.fe20e8cd.chunk.js"
+  },
+  {
+    "revision": "d86cb27f3bd4457fee1a",
+    "url": "/static/js/1.d86cb27f.chunk.js"
+  },
+  {
+    "revision": "a289f6c0cc2ba8b7fff5",
+    "url": "/static/css/2.80f56f9e.chunk.css"
+  },
+  {
+    "revision": "a289f6c0cc2ba8b7fff5",
+    "url": "/static/js/2.a289f6c0.chunk.js"
+  },
+  {
+    "revision": "5cea28c34b13c7fd6f26",
+    "url": "/static/js/runtime~main.5cea28c3.js"
+  },
+  {
+    "revision": "73c8dd3f9451f685c7d12beac6e9edf8",
+    "url": "/static/media/play.73c8dd3f.svg"
+  },
+  {
+    "revision": "2e4eb7b1ca2f5134b2fa3febd37ed8eb",
+    "url": "/static/media/search.2e4eb7b1.svg"
+  },
+  {
+    "revision": "1651bbf1e1628dbcf1ce5c8660fa686e",
+    "url": "/static/media/soundWaveR.1651bbf1.svg"
+  },
+  {
+    "revision": "62c167edb07ffd22397ec9663a58fc1a",
+    "url": "/static/media/soundWaveLW.62c167ed.svg"
+  },
+  {
+    "revision": "3eb6164051e26c1816f7c37aa7a9eab9",
+    "url": "/static/media/soundWaveLB.3eb61640.svg"
+  },
+  {
+    "revision": "8a3aec007479b5a67a4e9b112a85e3d8",
+    "url": "/static/media/Heart.8a3aec00.svg"
+  },
+  {
+    "revision": "ede5c9fc9e9f52868da28af917f72097",
+    "url": "/static/media/playMusic.ede5c9fc.svg"
+  },
+  {
+    "revision": "e91ecffb6ebbe4c7e39af799e33fd9e8",
+    "url": "/static/media/pause.e91ecffb.svg"
+  },
+  {
+    "revision": "b1f3e742443ad60a81ea9b936f7e88b2",
+    "url": "/static/media/download.b1f3e742.svg"
+  },
+  {
+    "revision": "393b42406a4e7b7f4252b851cbb3c13b",
+    "url": "/static/media/share.393b4240.svg"
+  },
+  {
+    "revision": "f51fc4ee8af703f7b24f2c09b97ad209",
+    "url": "/static/media/loop.f51fc4ee.svg"
+  },
+  {
+    "revision": "9f07eb5bed46bcb1709d20b94bc2e09e",
+    "url": "/static/media/previousSong.9f07eb5b.svg"
+  },
+  {
+    "revision": "ace7468bae517e3eae1e5476b5e44657",
+    "url": "/static/media/nextSong.ace7468b.svg"
+  },
+  {
+    "revision": "fe022eb0bd9939e131109e590dc36d7e",
+    "url": "/static/media/shuffle.fe022eb0.svg"
+  },
+  {
+    "revision": "931bb00104241cddfc49a2b0c702a70c",
+    "url": "/static/media/date.931bb001.svg"
+  },
+  {
+    "revision": "a2469de1496794b2ff5128f4a7931419",
+    "url": "/static/media/dl.a2469de1.svg"
+  },
+  {
+    "revision": "9e66a74285987db9bc0258d505253378",
+    "url": "/static/media/arrow.9e66a742.svg"
+  },
+  {
+    "revision": "28051fccc1b7fdd7a6122ebf1eb3f348",
+    "url": "/static/media/usa.28051fcc.svg"
+  },
+  {
+    "revision": "dc648f1495abcb80bfb77ceeb33b469f",
+    "url": "/index.html"
+  }
+];
